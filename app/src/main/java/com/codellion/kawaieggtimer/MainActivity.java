b2e975d421ca85d5c397be7ba83149c058cda374
khedubaba <adityakhedekar49@gmail.com>
2020-03-27 17:23:11 +0530

    c1: UI design completed.

---
+++ app/src/main/java/com/codellion/kawaieggtimer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.codellion.kawaieggtimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
